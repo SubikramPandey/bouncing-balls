c6669d0ba89b97bf85eeed8e25d8d9b1cec65815
Subikram Pandey <subikram@american.edu>
2017-03-07 12:48:14 -0500

    ball now gets smushed when it hits a side wall

--- first.js
+++ first.js
@@ -1,16 +1,17 @@
 
 var newX = 0;
-var ballSpeed = 7;  // how fast the ball moves in x direction
+var ballSpeed = 8;  // how fast the ball moves in x direction
 var change = 0;   // use to determine how much the ball shifts in y direction
 var ballArray = [];
 var hits = 1;
+var shapeWidth;
+var shapeHeight;
 
 function setup() {
-  createCanvas(600, 640);
+  createCanvas(600, 800);
   frameRate = 120;
- // var mySound = loadSound('assets/turndown.mp3');
-//  mySound.setVolume(1);
-// mySound.play();
+  shapeWidth = 60;
+  shapeHeight = 60;
 }
 
 function draw() {
@@ -22,13 +23,13 @@ function draw() {
 
 function drawBalls() {
   for (var i = 0; i < hits; i++) {
-    ellipse(updateX(), updateY(), 80, 80);
+    ellipse(updateX(), updateY(), shapeWidth, shapeHeight);
     fill(50 * i ,  100, 50 * (hits + i));
   };
 }
 
 function updateY() {
-  var y = sin(change) * 500 * change ;
+  var y = sin(change) * 500 * change  + 100;
   change += .02;
   if (change > 1.3) { change = - change};
   return y
@@ -38,7 +39,7 @@ function updateX(){
   newX += ballSpeed;
   if (newX > width || newX < 5 ){
     wallHitEffects();
-    return newX += ballSpeed
+    return newX 
   }
   return newX
 }
@@ -46,14 +47,21 @@ function updateX(){
 function wallHitEffects() {
   updateBallSpeed();
   hits += 1;
+  smushBall();
   if (hits > 10) {
     hits = 1;
     setup();
   }
 }
 
+function smushBall() {
+  shapeWidth -= 5;
+  shapeHeight += 5;
+  
+}
+
 function updateBallSpeed(){
-  ballSpeed = -1 * ( ballSpeed + .6);
+  ballSpeed = -1 * ( ballSpeed + .01);
   if (ballSpeed > 40) { ballSpeed = 5}
 }
 
