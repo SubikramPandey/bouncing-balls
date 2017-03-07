0ec21384a478a9a83efa1ca59a8d715856338f08
SubikramPandey <sp6171a@american.edu>
2017-03-07 00:20:58 -0500

    removed unnessisary code

--- first.js
+++ first.js
@@ -1,18 +1,17 @@
-var FR = 40;
+
 var counter = 0;
-var ballSpeed = 5;
-var change = 0;
+var ballSpeed = 5;  // how fast the ball moves in x direction
+var change = 0;   // use to determine how much the ball shifts in y direction
 var ballArray = [];
 var hits = 1;
 
 function setup() {
-  createCanvas(640, 480);
-  frameRate = FR;
+  createCanvas(800, 640);
+  frameRate = 120;
 }
 
 function draw() {
   background(0);
-  if (mouseIsPressed) {setup();}
   fill(255);
   
   for (var i = ballArray.length - 1; i >= 0; i--) {
@@ -21,19 +20,24 @@ function draw() {
   ellipse(updateX(), updateY(), 80, 80);
 }
 
+function updateY() {
+  var y = sin(change) * 500 * change ;
+  change += .02;
+  if (change > 1.3) { change = - change};
+  return y
+}
 
 function updateX(){
   counter += ballSpeed;
   if (counter > width || counter < 5 ){
-    ballSpeed = -ballSpeed;
-    setup();
+    wallHitEffects();
   }
   return counter
 }
 
-function updateY() {
-  var y = sin(change) * 500 * change ;
-  change += .02;
-  if (change > 1) { change = - change};
-  return y
+function wallHitEffects() {
+    ballSpeed = -ballSpeed;
+    hits += 1;
+    setup();
 }
+
